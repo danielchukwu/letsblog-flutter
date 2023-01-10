e02c21dd14d0df672208d97017393d261fe60af7
dc <00danzy@gmail.com>
2023-01-10 09:29:56 +0100

    first commit

---
+++ lib/models/Notifications.dart
@@ -0,0 +1,428 @@
+Map notificationsData = {
+  "seen": {
+    "last_month": [
+      [
+        {
+          "ago": "1 month ago",
+          "avatar": "nhytevizpbsibl3egk8b",
+          "group_id": "9fdb6a1b-a6ce-42bd-9609-740b086537bd",
+          "id": 142,
+          "junior_id": 7,
+          "leader_id": 4,
+          "seen": true,
+          "senior_id": 4,
+          "type": "follow",
+          "username": "dexter"
+        }
+      ],
+      [
+        {
+          "ago": "1 month ago",
+          "avatar": "tnqrdvsk2njmqpzxvrhf",
+          "group_id": "9a4f79be-23b0-42d6-9d0e-847f7b88070c",
+          "id": 40,
+          "junior_id": 5,
+          "leader_id": 4,
+          "seen": true,
+          "senior_id": 4,
+          "type": "follow",
+          "username": "imoney"
+        },
+        {
+          "ago": "1 month ago",
+          "avatar": "isakoloosr2ugdsd2iow",
+          "group_id": "9a4f79be-23b0-42d6-9d0e-847f7b88070c",
+          "id": 32,
+          "junior_id": 6,
+          "leader_id": 4,
+          "seen": true,
+          "senior_id": 4,
+          "type": "follow",
+          "username": "albert"
+        }
+      ],
+      [
+        {
+          "ago": "1 month ago",
+          "avatar": "nhytevizpbsibl3egk8b",
+          "blog_img": "pvf8gjt6azuqjunmqfjy",
+          "group_id": "7aefb586-5c34-4aec-b3e9-d43e500c0958",
+          "id": 146,
+          "junior_id": 7,
+          "leader_id": 4,
+          "seen": true,
+          "senior_id": 12,
+          "type": "liked_blog",
+          "username": "dexter"
+        }
+      ],
+      [
+        {
+          "ago": "1 month ago",
+          "avatar": "isakoloosr2ugdsd2iow",
+          "blog_img": "pvf8gjt6azuqjunmqfjy",
+          "group_id": "866835bb-52db-4c05-8cd1-f6c83c305718",
+          "id": 80,
+          "junior_id": 6,
+          "leader_id": 4,
+          "seen": true,
+          "senior_id": 12,
+          "type": "liked_blog",
+          "username": "albert"
+        },
+        {
+          "ago": "1 month ago",
+          "avatar": "aow8cnkhyb3hwmns41dl",
+          "blog_img": "pvf8gjt6azuqjunmqfjy",
+          "group_id": "866835bb-52db-4c05-8cd1-f6c83c305718",
+          "id": 61,
+          "junior_id": 4,
+          "leader_id": 4,
+          "seen": true,
+          "senior_id": 12,
+          "type": "liked_blog",
+          "username": "dandizzy"
+        }
+      ],
+      [
+        {
+          "ago": "1 month ago",
+          "avatar": "tnqrdvsk2njmqpzxvrhf",
+          "group_id": "58287faa-98cb-45d3-b51e-e12538d8380a",
+          "id": 152,
+          "junior_id": 5,
+          "leader_id": 4,
+          "seen": true,
+          "senior_id": 62,
+          "type": "liked_comment",
+          "username": "imoney"
+        },
+        {
+          "ago": "1 month ago",
+          "avatar": "isakoloosr2ugdsd2iow",
+          "group_id": "58287faa-98cb-45d3-b51e-e12538d8380a",
+          "id": 131,
+          "junior_id": 6,
+          "leader_id": 4,
+          "seen": true,
+          "senior_id": 59,
+          "type": "liked_comment",
+          "username": "albert"
+        }
+      ],
+      [
+        {
+          "ago": "1 month ago",
+          "avatar": "tnqrdvsk2njmqpzxvrhf",
+          "group_id": "e3222352-7001-4e79-b20b-d10abd5cbd28",
+          "id": 123,
+          "junior_id": 5,
+          "leader_id": 4,
+          "seen": true,
+          "senior_id": 59,
+          "type": "liked_comment",
+          "username": "imoney"
+        },
+        {
+          "ago": "1 month ago",
+          "avatar": "nhytevizpbsibl3egk8b",
+          "group_id": "e3222352-7001-4e79-b20b-d10abd5cbd28",
+          "id": 121,
+          "junior_id": 7,
+          "leader_id": 4,
+          "seen": true,
+          "senior_id": 1,
+          "type": "liked_comment",
+          "username": "dexter"
+        },
+        {
+          "ago": "1 month ago",
+          "avatar": "nhytevizpbsibl3egk8b",
+          "group_id": "e3222352-7001-4e79-b20b-d10abd5cbd28",
+          "id": 117,
+          "junior_id": 7,
+          "leader_id": 4,
+          "seen": true,
+          "senior_id": 62,
+          "type": "liked_comment",
+          "username": "dexter"
+        },
+        {
+          "ago": "1 month ago",
+          "avatar": "nhytevizpbsibl3egk8b",
+          "group_id": "e3222352-7001-4e79-b20b-d10abd5cbd28",
+          "id": 112,
+          "junior_id": 7,
+          "leader_id": 4,
+          "seen": true,
+          "senior_id": 30,
+          "type": "liked_comment",
+          "username": "dexter"
+        },
+        {
+          "ago": "1 month ago",
+          "avatar": "nhytevizpbsibl3egk8b",
+          "group_id": "e3222352-7001-4e79-b20b-d10abd5cbd28",
+          "id": 108,
+          "junior_id": 7,
+          "leader_id": 4,
+          "seen": true,
+          "senior_id": 59,
+          "type": "liked_comment",
+          "username": "dexter"
+        },
+        {
+          "ago": "1 month ago",
+          "avatar": "tnqrdvsk2njmqpzxvrhf",
+          "group_id": "e3222352-7001-4e79-b20b-d10abd5cbd28",
+          "id": 97,
+          "junior_id": 5,
+          "leader_id": 4,
+          "seen": true,
+          "senior_id": 1,
+          "type": "liked_comment",
+          "username": "imoney"
+        }
+      ],
+      [
+        {
+          "ago": "1 month ago",
+          "avatar": "nhytevizpbsibl3egk8b",
+          "blog_img": "pvf8gjt6azuqjunmqfjy",
+          "group_id": "c753280a-874a-4441-a81f-d2071ff87edd",
+          "id": 147,
+          "junior_id": 7,
+          "leader_id": 4,
+          "seen": true,
+          "senior_id": 12,
+          "type": "commented_blog",
+          "username": "dexter"
+        }
+      ],
+      [
+        {
+          "ago": "1 month ago",
+          "avatar": "tnqrdvsk2njmqpzxvrhf",
+          "blog_img": "cioir7pa4yjztnki7w7p",
+          "group_id": "24c7a7a0-c782-47c5-acb8-cdb5a5a236e4",
+          "id": 98,
+          "junior_id": 5,
+          "leader_id": 4,
+          "seen": true,
+          "senior_id": 28,
+          "type": "commented_blog",
+          "username": "imoney"
+        },
+        {
+          "ago": "1 month ago",
+          "avatar": "isakoloosr2ugdsd2iow",
+          "blog_img": "pvf8gjt6azuqjunmqfjy",
+          "group_id": "24c7a7a0-c782-47c5-acb8-cdb5a5a236e4",
+          "id": 82,
+          "junior_id": 6,
+          "leader_id": 4,
+          "seen": true,
+          "senior_id": 12,
+          "type": "commented_blog",
+          "username": "albert"
+        }
+      ],
+      [
+        {
+          "ago": "1 month ago",
+          "avatar": "nhytevizpbsibl3egk8b",
+          "group_id": "a76675f4-0e92-4288-8df3-2b2358efe0df",
+          "id": 144,
+          "junior_id": 7,
+          "leader_id": 4,
+          "seen": true,
+          "senior_id": 62,
+          "type": "commented_comment",
+          "username": "dexter"
+        }
+      ],
+      [
+        {
+          "ago": "1 month ago",
+          "avatar": "nhytevizpbsibl3egk8b",
+          "group_id": "577301c0-d9df-4c5c-8a83-c58b19f181b0",
+          "id": 60,
+          "junior_id": 7,
+          "leader_id": 4,
+          "seen": true,
+          "senior_id": 30,
+          "type": "commented_comment",
+          "username": "dexter"
+        }
+      ]
+    ],
+    "old": [],
+    "this_month": [
+      [
+        {
+          "ago": "3 weeks ago",
+          "avatar": "oyaewkauyrax9v3ie1ae",
+          "group_id": "9fdb6a1b-a6ce-42bd-9609-740b086537bd",
+          "id": 163,
+          "junior_id": 11,
+          "leader_id": 4,
+          "seen": true,
+          "senior_id": 4,
+          "type": "follow",
+          "username": "ab"
+        }
+      ],
+      [
+        {
+          "ago": "2 weeks ago",
+          "avatar": "nhytevizpbsibl3egk8b",
+          "blog_img": "cioir7pa4yjztnki7w7p",
+          "group_id": "58fcded7-a3d2-4cff-a41c-4c5e62732e2e",
+          "id": 208,
+          "junior_id": 7,
+          "leader_id": 4,
+          "seen": true,
+          "senior_id": 28,
+          "type": "liked_blog",
+          "username": "dexter"
+        }
+      ],
+      [
+        {
+          "ago": "3 weeks ago",
+          "avatar": "oyaewkauyrax9v3ie1ae",
+          "blog_img": "cioir7pa4yjztnki7w7p",
+          "group_id": "7aefb586-5c34-4aec-b3e9-d43e500c0958",
+          "id": 179,
+          "junior_id": 11,
+          "leader_id": 4,
+          "seen": true,
+          "senior_id": 28,
+          "type": "liked_blog",
+          "username": "ab"
+        },
+        {
+          "ago": "3 weeks ago",
+          "avatar": "isakoloosr2ugdsd2iow",
+          "blog_img": "cioir7pa4yjztnki7w7p",
+          "group_id": "7aefb586-5c34-4aec-b3e9-d43e500c0958",
+          "id": 170,
+          "junior_id": 6,
+          "leader_id": 4,
+          "seen": true,
+          "senior_id": 28,
+          "type": "liked_blog",
+          "username": "albert"
+        }
+      ],
+      [
+        {
+          "ago": "2 weeks ago",
+          "avatar": "nhytevizpbsibl3egk8b",
+          "group_id": "ac9d06aa-09c6-419a-b9d1-2df2b8227088",
+          "id": 201,
+          "junior_id": 7,
+          "leader_id": 4,
+          "seen": true,
+          "senior_id": 61,
+          "type": "liked_comment",
+          "username": "dexter"
+        },
+        {
+          "ago": "2 weeks ago",
+          "avatar": "nhytevizpbsibl3egk8b",
+          "group_id": "ac9d06aa-09c6-419a-b9d1-2df2b8227088",
+          "id": 199,
+          "junior_id": 7,
+          "leader_id": 4,
+          "seen": true,
+          "senior_id": 74,
+          "type": "liked_comment",
+          "username": "dexter"
+        }
+      ],
+      [
+        {
+          "ago": "3 weeks ago",
+          "avatar": "nhytevizpbsibl3egk8b",
+          "group_id": "20322df1-8fe4-44ac-b60c-2b68f5997134",
+          "id": 197,
+          "junior_id": 7,
+          "leader_id": 4,
+          "seen": true,
+          "senior_id": 75,
+          "type": "liked_comment",
+          "username": "dexter"
+        }
+      ],
+      [
+        {
+          "ago": "3 weeks ago",
+          "avatar": "oyaewkauyrax9v3ie1ae",
+          "group_id": "58287faa-98cb-45d3-b51e-e12538d8380a",
+          "id": 173,
+          "junior_id": 11,
+          "leader_id": 4,
+          "seen": true,
+          "senior_id": 30,
+          "type": "liked_comment",
+          "username": "ab"
+        },
+        {
+          "ago": "3 weeks ago",
+          "avatar": "oyaewkauyrax9v3ie1ae",
+          "group_id": "58287faa-98cb-45d3-b51e-e12538d8380a",
+          "id": 156,
+          "junior_id": 11,
+          "leader_id": 4,
+          "seen": true,
+          "senior_id": 59,
+          "type": "liked_comment",
+          "username": "ab"
+        }
+      ],
+      [
+        {
+          "ago": "2 weeks ago",
+          "avatar": "nhytevizpbsibl3egk8b",
+          "group_id": "d489828e-ccae-45e8-92d5-52bcde72728b",
+          "id": 202,
+          "junior_id": 7,
+          "leader_id": 4,
+          "seen": true,
+          "senior_id": 74,
+          "type": "commented_comment",
+          "username": "dexter"
+        }
+      ]
+    ],
+    "this_week": [
+      [
+        {
+          "ago": "6 days ago",
+          "avatar": "tnqrdvsk2njmqpzxvrhf",
+          "group_id": "bd85c4ce-bc7d-4786-8e28-2eb246eb422d",
+          "id": 221,
+          "junior_id": 5,
+          "leader_id": 4,
+          "seen": true,
+          "senior_id": 75,
+          "type": "liked_comment",
+          "username": "imoney"
+        }
+      ]
+    ],
+    "this_year": [],
+    "today": [],
+    "yesterday": []
+  },
+  "unseen": {
+    "last_month": [],
+    "old": [],
+    "this_month": [],
+    "this_week": [],
+    "this_year": [],
+    "today": [],
+    "yesterday": []
+  }
+};
