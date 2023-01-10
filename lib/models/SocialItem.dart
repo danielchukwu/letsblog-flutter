c050eb7d79da28d6af0ef209e03eb4c6081e6a22
dc <00danzy@gmail.com>
2023-01-10 09:30:30 +0100

    first commit

---
+++ lib/models/SocialItem.dart
@@ -0,0 +1,12 @@
+class SocialItems {
+  static List<String> socialsList = ['instagram','facebook','twitter','www','linkedin','youtube',];
+
+  static Map items= {
+    'instagram': 'assets/icons/instagram.svg',
+    'facebook':  'assets/icons/facebook.svg',
+    'twitter':   'assets/icons/twitter.svg',
+    'www':       'assets/icons/www.svg',
+    'linkedin':  'assets/icons/linkedin.svg',
+    'youtube':   'assets/icons/youtube.svg',
+  };
+}
